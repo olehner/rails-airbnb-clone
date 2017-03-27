792a9aae01660a41edef430b0353d005890b6311
Oliver Lehner <lehner.oliver@gmail.com>
2017-03-27 19:12:58 +0200

    added reviews including polymorphic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327162703) do
+ActiveRecord::Schema.define(version: 20170327170732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,18 @@ ActiveRecord::Schema.define(version: 20170327162703) do
     t.index ["user_id"], name: "index_parking_spots_on_user_id", using: :btree
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "rating"
+    t.integer  "booking_id"
+    t.string   "reviewable_type"
+    t.integer  "reviewable_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
+    t.index ["reviewable_type", "reviewable_id"], name: "index_reviews_on_reviewable_type_and_reviewable_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "first_name"
@@ -64,4 +76,5 @@ ActiveRecord::Schema.define(version: 20170327162703) do
   add_foreign_key "bookings", "users"
   add_foreign_key "parking_spots", "addresses"
   add_foreign_key "parking_spots", "users"
+  add_foreign_key "reviews", "bookings"
 end
