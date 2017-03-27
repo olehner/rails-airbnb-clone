100aabfec0d96d79d1d90efa86bdf7fca0414556
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2017-03-27 17:49:21 +0200

    Created user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170327154744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "date_of_birth"
+    t.text     "bio"
+    t.string   "phone_number"
+    t.boolean  "is_parklord"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
 end
