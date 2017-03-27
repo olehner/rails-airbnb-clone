2773c0aab8e7e3d589779f6bd92976d53c1deaed
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-27 18:01:22 +0200

    added address model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327154744) do
+ActiveRecord::Schema.define(version: 20170327160022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "street_name"
+    t.string   "street_number"
+    t.string   "zip_code"
+    t.string   "city"
+    t.string   "country"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
