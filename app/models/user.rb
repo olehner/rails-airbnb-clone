cece4d271c5f813d21dd67e4d7e30baf00ac7a02
Oliver Lehner <lehner.oliver@gmail.com>
2017-03-28 11:22:58 +0200

    fixed typo of default on is_parklord, added link to reviews as reviewable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   belongs_to :account
   has_many :comments
   belongs_to :address
+  has_many :reviews, as: :reviewable
   validates :username, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :date_of_birth, presence: true
   validates :phone_number, presence: true
-  validates :is_parklord, presence: true, defaul: false
+  validates :is_parklord, presence: true, default: false
 end
