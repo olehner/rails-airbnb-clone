630a1bdcfc101bfacbb174a95aabd9109fd15539
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-28 11:04:46 +0200

    added associations on models parking spot and booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :parking_spot
+  has_many :comments
   has_many :reviews, :as => :reviewable
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :status, presence: true, inclusion: { in: ["pending", "confirmed", "cancelled", "completed"], allow_nil: false }
 end
