22ec88628f5b644e8d9bf03483552a6ed4952019
Oliver Lehner <lehner.oliver@gmail.com>
2017-03-29 22:28:27 +0200

    added search bar in navbar - search by city - leads to index page for spots in city

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # for everyone
+  get 'parking_spots/search', to: 'parking_spots#citysearch'
+
   resources :parking_spots, only: [:index, :show]
 
   # for logged in users
