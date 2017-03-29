f960d36eedb90ebd3c32e34e4c00f56ff77790d6
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-29 12:57:30 +0200

    added sign-in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,19 +4,6 @@ class UsersController < ApplicationController
   def show
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
-
   def edit
 
   end
