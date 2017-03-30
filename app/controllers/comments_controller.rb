29cc0bd1a0e79b8c8c22f06b22e1d3f10e2b3bc5
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-30 16:02:20 +0200

    made booking controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_account!
+
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @comment = Comment.create(comment_params)
+    @comment.user_id = current_user.id
+    @comment.booking_id = @booking.id
+
+    if @comment.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
